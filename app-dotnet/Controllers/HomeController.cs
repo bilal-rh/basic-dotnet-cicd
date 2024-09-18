cccb8375ba3606a839c27269144dea932f64ab4f
bilal-rh <bilal.ashraf@live.com>
2024-09-18 15:46:24 +0400

    fixing wrong header

--- app-dotnet/Controllers/HomeController.cs
+++ app-dotnet/Controllers/HomeController.cs
@@ -8,11 +8,11 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 
     public IActionResult Index()
     {
